53a121665472d3bb5e3e30dc1ae0085e177a1fee
slash <zsq276987359@gmail.com>
2025-03-29 00:33:50 +0800

    chore: trayicon demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,90 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
+import { TrayIcon } from "@tauri-apps/api/tray";
+import { Menu, MenuItem, PredefinedMenuItem } from "@tauri-apps/api/menu";
+import { resolveResource } from "@tauri-apps/api/path";
+
+const TRAY_ID = "app-tray";
 
 export default function Home() {
+  useEffect(() => {
+    let trayInstance: TrayIcon | null = null;
+
+    const initTray = async () => {
+      try {
+        // 检查是否已存在托盘实例
+        trayInstance = await TrayIcon.getById(TRAY_ID);
+        console.log("trayInstance", trayInstance);
+
+        if (trayInstance) {
+          return;
+        }
+
+        const menu = await Menu.new();
+
+        // 添加菜单项
+        await menu.append(
+          await MenuItem.new({
+            text: "设置",
+            action: () => {
+              TrayIcon.removeById(TRAY_ID);
+              console.log("打开设置");
+            },
+          })
+        );
+
+        // await menu.append(PredefinedMenuItem.new({ item: "Separator" }));
+
+        await menu.append(
+          await MenuItem.new({
+            text: "检查更新",
+            action: () => {
+              console.log("检查更新");
+            },
+          })
+        );
+
+        // await menu.append(await PredefinedMenuItem.new("separator"));
+
+        await menu.append(
+          await MenuItem.new({
+            text: "退出",
+            action: () => {
+              console.log("退出应用");
+            },
+          })
+        );
+
+        // 创建托盘图标
+        const iconPath = "./icons/tray-mac.ico";
+        const icon = await resolveResource(iconPath);
+
+        console.log("icon", icon);
+
+        trayInstance = await TrayIcon.new({
+          menu,
+          icon,
+          id: TRAY_ID,
+          tooltip: "我的应用",
+          iconAsTemplate: true,
+          menuOnLeftClick: true,
+        });
+      } catch (error) {
+        console.error("创建托盘失败:", error);
+      }
+    };
+
+    initTray();
+
+    // 清理函数
+    return () => {
+      if (trayInstance) {
+        TrayIcon.removeById(TRAY_ID);
+      }
+    };
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
